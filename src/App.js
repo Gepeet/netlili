1ab07bef61d9806d7cb71f5e9d0a860b49493aaa
VenGallardo <letskeddotcom@gmail.com>
2021-09-12 21:35:15 +0800

    3rd push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
     <div className="body">
       <div className="card">
         <h1>Sample</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, enim!</p>
+        <p style={{
+          position:'block'
+        }}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, enim!</p>
         <button className="btn">Click me</button>
       </div>
     </div>
