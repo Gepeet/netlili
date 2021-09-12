9ed1bbe861d23d9b1cfd42c8bd8c8a2a6786d9c7
VenGallardo <letskeddotcom@gmail.com>
2021-09-12 21:21:07 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+      <div className="card">
+        <h1>Sample</h1>
+        <button className="btn">Click me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
