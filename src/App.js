bb372e619b0b853aca908d5174be0638a4973a05
VenGallardo <letskeddotcom@gmail.com>
2021-09-17 10:21:54 +0800

    change the body width to max 1400px

--- src/App.js
+++ src/App.js
@@ -5,38 +5,88 @@ import Hero from './Component/Hero/Hero';
 import Mission from './Component/Mission/Mission';
 import Dropdown from './Component/Nav/Dropdown';
 import Stories from './Component/Stories/Stories';
+import React from 'react';
+// import HamDrop from './Component/Nav/hamDrop/HamDrop';
+import {BrowserRouter} from 'react-router-dom';
+import Modal from 'react-modal';
+import HamDrop from './Component/Nav/hamDrop/HamDrop';
 
+const customStyles = {
+  overlay:{
+    background:'none',
+  },
+  content: {
+    margin:'0',
+    padding:'0',
+    top: '0%',
+    left: '0%',
+    width:'100vw',
+    height:'100vh',
+    background:'none'
+    // marginRight: '-50%',
+    // transform: 'translate(-50%, -50%)',
+    // zIndex:'1000'
+  },
+};
 
-function App() {
+function App(){
+
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="body">
-      <nav>
-        <h3>Adron</h3>
-        <Dropdown/>
-            <div className="carts">
-                <h4>FR</h4>
-                <span>Cart</span>
-                <div className="cart__value">
-                  <span>0</span>
+    <BrowserRouter>
+      <div className={!modalIsOpen ? 'body' : 'body deactive'}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        
+      >
+        <div className={!modalIsOpen ? 'hamdrop': 'hamdropactive'}>
+          <HamDrop/>
+        </div>
+        
+      </Modal>
+        
+        <nav>
+          <h3>Adron</h3>
+          <Dropdown/>
+              <div className="carts">
+                  <h4>FR</h4>
+                  <span>Cart</span>
+                  <div className="cart__value">
+                    <span>0</span>
+                  </div>
+                  <div className="hamburger" onClick={openModal}>
+                    <span></span>
+                    <span></span>
                 </div>
-                <div className="hamburger">
-                  <span></span>
-                  <span></span>
               </div>
-            </div>
-            
-      </nav>
-      <div className="hero__container">
-        <Hero/>
+              
+        </nav>
+        <div className="hero__container">
+          <Hero/>
+        </div>
+        <div className="feat">
+        <Featured/>
+        </div>
+        <Mission/>
+        <Stories/>
+        <Community/>
       </div>
-      <div className="feat">
-      <Featured/>
-      </div>
-      <Mission/>
-      <Stories/>
-      <Community/>
-    </div>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ src/Component/Nav/hamDrop/HamDrop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hamdrop.css';
+
+function HamDrop() {
+    return (
+        <div className="hamdrop">
+            Nav drop
+        </div>
+    )
+}
+
+export default HamDrop
